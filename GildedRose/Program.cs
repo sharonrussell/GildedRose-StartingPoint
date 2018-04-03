c606df0771c40216666e297af70cc0739e2ec2d7
Sharon Russell <sharon.russell@comparethemarket.com>
2018-04-03 11:25:03 +0100

    Initial commit for GildedRose kata starting point

---
+++ GildedRose/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GildedRose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Inventory inventory = new Inventory();
+            
+            foreach (var item in inventory.Items)
+            {
+                Console.WriteLine(item.Name + "... \n");
+                Console.WriteLine(item.Quality + "... \n");
+                Console.WriteLine(item.SellIn + "... \n");
+            }
+            
+            inventory.UpdateQuality();
+
+            Console.WriteLine("...UPDATING... \n \n");
+            
+            foreach (var item in inventory.Items)
+            {
+                Console.WriteLine(item.Name + "... \n");
+                Console.WriteLine(item.Quality + "... \n");
+                Console.WriteLine(item.SellIn + "... \n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
